b742181811d074923a1c3d82421d104cac9b35d7
icancode23 <ernipunarora@gmail.com>
2017-06-13 01:26:19 +0530

    changed file path

--- SynchronousMonitorApache.sh
+++ SynchronousMonitorApache.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 while inotifywait -e modify /var/log/apache2/access.log; do
-  if tail -n1 /var/log/apache2/access.log | grep httpd; then
+  # if tail -n1 /var/log/apache2/access.log | grep httpd; then
     kdialog --msgbox "Hello From Apache!"
-  fi
+  # fi
 done
